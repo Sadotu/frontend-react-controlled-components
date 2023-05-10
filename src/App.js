fc223954e8622f4b6ae349f90b512f29939547e5
Nick <nickdekok.nl@gmail.com>
2023-05-10 15:11:05 +0200

    opdracht gemaakt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      Maak hier jouw formulier!
-    </div>
-  );
-}
+const Formulier = () => {
+    const [naam, setNaam] = useState('');
+    const [leeftijd, setLeeftijd] = useState('');
+    const [review, setReview] = useState('');
+    const [nieuwsbrief, setNieuwsbrief] = useState(false);
 
-export default App;
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Naam:', naam);
+        console.log('Leeftijd:', leeftijd);
+        console.log('Review:', review);
+        console.log('Nieuwsbrief:', nieuwsbrief);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Naam:
+                <input
+                    type="text"
+                    value={naam}
+                    onChange={(e) => setNaam(e.target.value)}
+                />
+            </label>
+            <br />
+            <label>
+                Leeftijd:
+                <input
+                    type="number"
+                    value={leeftijd}
+                    onChange={(e) => setLeeftijd(e.target.value)}
+                />
+            </label>
+            <br />
+            <label>
+                Jouw Review:
+                <textarea
+                    value={review}
+                    onChange={(e) => setReview(e.target.value)}
+                />
+            </label>
+            <br />
+            <label>
+                Inschrijven voor nieuwsbrief:
+                <input
+                    type="checkbox"
+                    checked={nieuwsbrief}
+                    onChange={(e) => setNieuwsbrief(e.target.checked)}
+                />
+            </label>
+            <br />
+            <button type="submit">Verzenden</button>
+        </form>
+    );
+};
+
+export default Formulier;
+
