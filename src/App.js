e9e65192118b9b56f2b777483f7ada494bb98185
Nick <nickdekok.nl@gmail.com>
2023-05-11 09:16:46 +0200

    implement react hook form library

--- src/App.js
+++ src/App.js
@@ -1,72 +1,62 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-//import { useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 const Formulier = () => {
-    const [ formState, setFormState ] = useState({
-        naam: '',
-        leeftijd: '',
-        review: '',
-        niewsbrief: false
-    })
+    const { register, handleSubmit, reset } = useForm();
 
-    function handleChange(event) {
-        const changedFieldName = event.target.name;
-        const newValue = event.target.type === "checkbox" ? event.target.checked : event.target.value;
-
-        setFormState({
-            ...formState,
-            [changedFieldName]: newValue,
-        });
+    function handleFormSubmit(data) {
+        console.log(data);
+        reset();
     }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(formState)
-    };
-
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Naam:
-                <input
-                    type="text"
-                    name="naam"
-                    value={formState.naam}
-                    onChange={handleChange}
-                />
-            </label>
-            <br />
-            <label>
-                Leeftijd:
-                <input
-                    type="number"
-                    name="leeftijd"
-                    value={formState.leeftijd}
-                    onChange={handleChange}
-                />
-            </label>
-            <br />
-            <label>
-                Jouw Review:
-                <textarea
-                    name="review"
-                    value={formState.review}
-                    onChange={handleChange}
-                />
-            </label>
-            <br />
-            <label>
-                Inschrijven voor nieuwsbrief:
-                <input
-                    type="checkbox"
-                    checked={formState.nieuwsbrief}
-                    onChange={handleChange}
-                />
-            </label>
-            <br />
-            <button type="submit">Verzenden</button>
-        </form>
+        <div className="container">
+            <form className="form" onSubmit={handleSubmit(handleFormSubmit)}>
+                <div className="form-section">
+                    <label>
+                        Naam:
+                        <input
+                            type="text"
+                            {...register("naam")}
+                            //value={formState.naam}
+                            //onChange={handleChange}
+                        />
+                    </label>
+                    <label>
+                        Leeftijd:
+                        <input
+                            type="number"
+                            {...register("leeftijd")}
+                            //value={formState.leeftijd}
+                            //onChange={handleChange}
+                        />
+                    </label>
+                </div>
+
+                <div className="form-section">
+                    <label>
+                        Jouw Review:
+                        <textarea
+                            {...register("review")}
+                            //value={formState.review}
+                            //onChange={handleChange}
+                        />
+                    </label>
+                    <label className="nieuwsbrief">
+                        Inschrijven voor nieuwsbrief:
+                        <input
+                            type="checkbox"
+                            {...register("nieuwsbrief")}
+                            //checked={formState.nieuwsbrief}
+                            //onChange={handleChange}
+                        />
+                    </label>
+                </div>
+
+                <button type="submit">Verzenden</button>
+            </form>
+        </div>
     );
 };
 
